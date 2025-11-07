52159b7a211059313fa03f0120bbddd0c150d062
bignarem <bignarem@gmail.com>
2025-11-07 12:28:09 -0400

    feat: Add category section
    This commit adds a new section to the home screen to display a list of doctor specialties (categories).
    
    This change includes:
    - A new `CategoryRow` Composable that displays a horizontal list of categories.
    - A `CategoryItem` Composable to represent each individual category with an icon and name.
    - Integration of `CategoryRow` into the `MainScreen`.
    - Logic in `MainScreen` to observe and load category data from the `MainViewModel`.
    - The addition of the `runtime-livedata` dependency to observe LiveData as state in Compose.

--- app/src/main/java/net/nesweb/doctorappointment/feature/home/MainScreen.kt
+++ app/src/main/java/net/nesweb/doctorappointment/feature/home/MainScreen.kt
@@ -3,15 +3,28 @@ package net.nesweb.doctorappointment.feature.home
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import net.nesweb.doctorappointment.core.ViewModel.MainViewModel
 
 @Composable
-fun MainScreen() {
+fun MainScreen(
+    viewModel: MainViewModel
+
+) {
+    val categories by viewModel.category.observeAsState(initial = emptyList())
+
+    LaunchedEffect(Unit) {
+        if (categories.isEmpty()) viewModel.loadCategory()
+    }
+
     var selectedBottom by remember { mutableStateOf(0) }
 
     Scaffold(
@@ -27,6 +40,7 @@ fun MainScreen() {
             item { HomeHeader() }
             item { Banner() }
             item { SectionHeader(title = "Doctor Speciality", onSeeAll = null) }
+            item { CategoryRow(items = categories) }
 
         }
     }
@@ -35,5 +49,6 @@ fun MainScreen() {
 @Preview
 @Composable
 fun MainScreenPreview() {
-    MainScreen()
+    val viewModel: MainViewModel = viewModel()
+    MainScreen(viewModel = viewModel)
 }
