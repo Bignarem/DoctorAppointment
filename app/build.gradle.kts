4ed208de429c8d558733be34edab41808cef81cc
Bignarem <35050327+Bignarem@users.noreply.github.com>
2025-11-19 19:32:03 -0400

    feat: Add Activity KTX dependency
    This commit adds the AndroidX Activity KTX library to the project.
    
    This change includes:
    - Adding `activity-ktx` to the `libs.versions.toml` file.
    - Including the `activity-ktx` dependency in the `app/build.gradle.kts` file.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.firebase.database)
     implementation(libs.androidx.compose.runtime.livedata)
+    implementation(libs.activity.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
